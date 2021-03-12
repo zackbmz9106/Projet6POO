3b1ceb19e3fda349da9f5b3eb20483f6ca7fbf79
FicheeSS <theo.ange.bocquet@gmail.com>
2021-03-12 16:21:49 +0100

    Help meee

--- src/projet6/mainwindow/MainWindow.java
+++ src/projet6/mainwindow/MainWindow.java
@@ -1,25 +1,42 @@
 package projet6.mainwindow;
 
 
+import java.awt.event.*;
+
 import javax.swing.*;
 
-public class MainWindow {
+public class MainWindow extends JFrame implements ActionListener {
 
-	public static void main(String[] args) {
-		JFrame mainwindow = new JFrame();
-		mainwindow.setSize(800,600);
-		mainwindow.setTitle("App");
-		JMenuBar menuBar = new JMenuBar();
-		mainwindow.setJMenuBar(menuBar);
-		JMenu menu = new JMenu("Fichier");
-		menuBar.add(menu); 
-		JMenuItem Help = new JMenuItem();
-		Help.addActionListener(null);
-		menu.add(Help);
-		mainwindow.setVisible(true);
+	JFrame MainWindow;
+	public static void  main(String[] args) {
+		new MainWindow();
+	}
 		
 
+	public  MainWindow() {
+		MainWindow = new JFrame();
+		MainWindow.setSize(800,600);
+		MainWindow.setTitle("App");
+		JMenuBar menuBar = new JMenuBar();
+		MainWindow.setJMenuBar(menuBar);
+		JMenu menu = new JMenu("Fichier");
+		menuBar.add(menu); 
+		JMenuItem Help = new JMenuItem("Help");
+		Help.addActionListener(this);
+		menu.add(Help);
+		MainWindow.setVisible(true);
 	}
-
+	
+    public  void    actionPerformed(ActionEvent e)
+    
+    {	
+    	switch(e.getActionCommand() ) {
+    	
+    	case "Help" :
+    		JOptionPane.showMessageDialog(MainWindow,
+    			    "Créé par Theo et Zakari");
+    	}
+        
+    }
 }
  
