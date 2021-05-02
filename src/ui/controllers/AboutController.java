0d5f9f8338a787432ade8c2f14d7b3fbac8939b6
FicheeSS <theo.ange.bocquet@gmail.com>
2021-05-03 00:31:10 +0200

    fix libs

--- src/ui/controllers/AboutController.java
+++ src/ui/controllers/AboutController.java
@@ -7,6 +7,8 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class AboutController extends ShowHideDialog implements Initializable {
+    
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
